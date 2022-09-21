6f68983d4b77edf6ce901d2947fb181fbe8381b0
spuerta <puertasantiago2002@gmail.com>
2022-09-21 17:50:29 -0500

    añadir modelo a GitHub

---
+++ mlapp/petclassifier/models.py
@@ -0,0 +1,11 @@
+from doctest import debug_script
+from platform import architecture
+from django.db import models
+
+# Create your models here.
+class mlModels(models.Model):
+    title = models.CharField(max_length=50)
+    description=models.CharField(max_length=250)
+    architecture = models.FileField(upload_to='mlmodels/') 
+    weights = models.FileField(upload_to='mlmodels/')
+    priority = models.PositiveSmallIntegerField(null = True)
